4e2e2158c5e91ff1cae2388e5a8b4bd40263d303
AMS2302 <ams23post@gmail.com>
2021-03-09 23:51:48 +0300

    init: implement solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (Array.isArray(array) && array.length > 0) {
+  return Math.min(...array); 
+  }
+  else {return 0}
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (Array.isArray(array) && array.length > 0) {
+    return Math.max(...array);
+  }
+  else {return 0}
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (Array.isArray(array) && array.length > 0) {
+  let i;
+  let x = 0;
+  for (i = 0; i < array.length; i++) {
+    x = x + array[i]
+  }
+  return (x / array.length);}
+  else {return 0}
 }
